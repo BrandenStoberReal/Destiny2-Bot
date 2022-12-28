e2369e6f2fdc882972c5b2770c591d3b61c75149
Branden Stober <67252380+BrandenStoberReal@users.noreply.github.com>
2022-12-27 20:03:04 -0500

    Refactoring

--- events/start.js
+++ events/start.js
@@ -1,6 +1,6 @@
 module.exports = {
-	name: 'ready',
-	async execute(interaction, client) {
-		console.log('Logged into ' + client.user.tag + '!');
-	},
+  name: "ready",
+  async execute(interaction, client) {
+    console.log("Logged into " + client.user.tag + "!");
+  },
 };
--- index.js
+++ index.js
@@ -1,57 +1,56 @@
 /* eslint-disable no-inline-comments */
 // This is the entry point for the entire bot.
 // Dependencies included: js, js voice, bufferutil, utf-8-decode, and zlib.
-const Env = require('dotenv');
-const { Client, GatewayIntentBits, Partials } = require('discord.js');
-const path = require('path');
-const Handler = require('discord-handlers');
-const { Collection } = require('mongoose');
+const Env = require("dotenv");
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const Handler = require("discord-handlers");
+const { Collection } = require("mongoose");
 const handler = new Handler();
 
 const client = new Client({
-	typescript: false,
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMembers,
-		GatewayIntentBits.GuildBans,
-		GatewayIntentBits.GuildEmojisAndStickers,
-		GatewayIntentBits.GuildIntegrations,
-		GatewayIntentBits.GuildWebhooks,
-		GatewayIntentBits.GuildInvites,
-		GatewayIntentBits.GuildVoiceStates,
-		GatewayIntentBits.GuildPresences,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.GuildMessageReactions,
-		GatewayIntentBits.GuildMessageTyping,
-		GatewayIntentBits.DirectMessages,
-		GatewayIntentBits.DirectMessageReactions,
-		GatewayIntentBits.DirectMessageTyping,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildScheduledEvents,
-		GatewayIntentBits.AutoModerationConfiguration,
-		GatewayIntentBits.AutoModerationExecution,
-	],
-	partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+  typescript: false,
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildBans,
+    GatewayIntentBits.GuildEmojisAndStickers,
+    GatewayIntentBits.GuildIntegrations,
+    GatewayIntentBits.GuildWebhooks,
+    GatewayIntentBits.GuildInvites,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.DirectMessageTyping,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildScheduledEvents,
+    GatewayIntentBits.AutoModerationConfiguration,
+    GatewayIntentBits.AutoModerationExecution,
+  ],
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction],
 });
 
 // Process the .env file
-Env.config({ path: './.env' });
+Env.config({ path: "./.env" });
 
 // Main Script Variables
 const Token = process.env.TOKEN;
 client.login(Token);
 
 // Handler Events
-handler.handleClientEvents('./events', client);
+handler.handleClientEvents("./events", client);
 handler.handleGlobalCommands(
-	'./commands',
-	client,
-	'1044374073624494161',
-	Token,
+  "./commands",
+  client,
+  "1044374073624494161",
+  Token
 );
 
 // Assign Global Variables
-global.admins = ['959826700236099614'];
-global.embedcolor = 'BLUE';
+global.admins = ["959826700236099614"];
+global.embedcolor = "BLUE";
 global.client = client;
 global.commands = new Collection();
